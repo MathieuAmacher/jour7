7a70186ad23b8eeca947e874f0f86c1638489b12
mathieu.amacher <amacher_mathieu@hotmail.com>
2023-10-03 17:00:32 +0200

    amélioration

--- exo_16.rb
+++ exo_16.rb
@@ -1,6 +1,16 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux tu ?"
+puts "Il faut un chiffre entre 1 et 25"
 print ">..."
 stage = gets.chomp.to_i
+
+while stage <=0 || stage >25
+
+    puts "ce n'est pas un chiffre compris entre 1 et 25"
+    puts "Tu peux recommencer"
+    print ">..."
+    stage = gets.chomp.to_i
+
+end
 
 puts "Voici la pyramide :"
 
--- exo_17.rb
+++ exo_17.rb
@@ -1,10 +1,22 @@
 puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux tu ?"
-print ">..."
-stage = gets.chomp.to_i
+puts "Choisis un nombre entre 1 et 25"
+    print ">..."
+    stage = gets.chomp.to_i
 
+    while stage <=0 || stage >25
+
+        puts "ce n'est pas un chiffre compris entre 1 et 25"
+        puts "Tu peux recommencer"
+        print ">..."
+        stage = gets.chomp.to_i
+    
+    end
+        
+        
+    
 puts "Voici la pyramide :"
 
-stage .times do |i|
+    stage .times do |i|
 
     print (" " * (stage-(i+1)))
 
@@ -12,6 +24,8 @@ stage .times do |i|
             puts "#" * (i+1)
         else 
             puts "#" * ((i+0.5)*2)
-        end
-
+   
+    end
+    
 end
+
